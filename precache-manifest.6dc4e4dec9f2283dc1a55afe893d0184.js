ac5efebf4deef5572360d91a553e76b4fd620f7e
Mo Zongran <mozongran@gmail.com>
2019-11-16 23:13:47 +0800

    Updates

---
+++ precache-manifest.6dc4e4dec9f2283dc1a55afe893d0184.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9da812d7ad821b0466f16fdbb5c3302f",
+    "url": "/HopeNUS/examtt-frontend-react/index.html"
+  },
+  {
+    "revision": "30b4ae29859976f393d5",
+    "url": "/HopeNUS/examtt-frontend-react/static/css/2.d5d9dee5.chunk.css"
+  },
+  {
+    "revision": "30b4ae29859976f393d5",
+    "url": "/HopeNUS/examtt-frontend-react/static/js/2.d15d5e1a.chunk.js"
+  },
+  {
+    "revision": "7bf23c5d73af40886a84",
+    "url": "/HopeNUS/examtt-frontend-react/static/js/main.4abea594.chunk.js"
+  },
+  {
+    "revision": "dc08f46d1b89994eff7e",
+    "url": "/HopeNUS/examtt-frontend-react/static/js/runtime-main.2b099fed.js"
+  }
+]);
