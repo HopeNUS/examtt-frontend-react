72000ca9a60ffeb8a20599fadac3e9e36a79475c
Mo Zongran <mozongran@gmail.com>
2019-11-16 21:10:17 +0800

    Change title

--- src/App.js
+++ src/App.js
@@ -1,13 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { MainContainer } from "./containers/MainContainer";
 
-function App() {
-  return (
-    <div className="App">
-        <p>Hello world.</p>
-    </div>
-  );
-}
-
-export default App;
+export const App = () => <MainContainer />
