d8d72c516f71499335643a271f6190915c909092
jeremiah-ang <jeremiahang94@gmail.com>
2019-11-16 20:21:33 +0800

    Set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+// Html Webpack: This plugin generates an HTML file with <script> injected
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'react_main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [htmlPlugin],
+
+};
